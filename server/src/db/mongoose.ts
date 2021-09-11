e25d8625763e8b03dd883751c9e0f380265c9c2a
joxpulp <joxpulp@gmail.com>
2021-09-11 18:26:55 -0500

    Heroku

--- server/src/db/mongoose.ts
+++ server/src/db/mongoose.ts
@@ -2,7 +2,9 @@ import { connect } from 'mongoose';
 
 export const mongoose = async (): Promise<void> => {
 	try {
-		await connect('mongodb://josu:josu@127.0.0.1:27017/ecommerce');
+		await connect(
+			'mongodb://josu:josu@cluster0-shard-00-00.jbg6c.mongodb.net:27017,cluster0-shard-00-01.jbg6c.mongodb.net:27017,cluster0-shard-00-02.jbg6c.mongodb.net:27017/ecommerce?replicaSet=atlas-ifxfb3-shard-0&ssl=true&authSource=admin'
+		);
 		console.log('Conectado a base de datos');
 	} catch (error) {
 		console.log(error);
