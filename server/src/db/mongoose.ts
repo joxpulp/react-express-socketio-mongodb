1c1f841d0fafc43a935c795625f2051c32eadd31
joxpulp <joxpulp@gmail.com>
2021-09-30 16:08:42 -0500

    feat: environment variable added

--- server/src/db/mongoose.ts
+++ server/src/db/mongoose.ts
@@ -1,9 +1,12 @@
 import { connect } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const mongoose = async (): Promise<void> => {
 	try {
 		await connect(
-			'mongodb://josu:josu@cluster0-shard-00-00.jbg6c.mongodb.net:27017,cluster0-shard-00-01.jbg6c.mongodb.net:27017,cluster0-shard-00-02.jbg6c.mongodb.net:27017/ecommerce?replicaSet=atlas-ifxfb3-shard-0&ssl=true&authSource=admin'
+			process.env.MONGO_URL || 'mongo_url'
 		);
 		console.log('Conectado a base de datos');
 	} catch (error) {
